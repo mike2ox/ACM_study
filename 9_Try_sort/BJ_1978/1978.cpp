24f64c0e8b9ddb7a97e3abf1b6cc9165073d3341
moonhyeok.song <firefinger07@gmail.com>
2018-03-19 18:21:15 +0900

    [BJ]1427

---
+++ 9_Try_sort/BJ_1978/1978.cpp
@@ -0,0 +1,36 @@
+/*
+*	author : quisutdeus7
+*	BJO : no. 1978
+*/
+#include<iostream>
+using namespace std;
+
+int N;
+int n_arr[1000] = {};
+int check =0;
+
+
+int main() {
+
+	cin >> N;
+
+	//input data
+	for(int a = 0; a < N; a++) 
+		cin >> n_arr[a];
+
+	// find prime number
+	for (int b = 0; b < N; b++) {
+		if (n_arr[b] > 1) {
+			for (int c = 2; c < n_arr[b]; c++) {
+				if (n_arr[b] % c == 0)
+					break;
+			}
+			check++;
+		}
+	}
+	cout << check;
+
+
+	return 0;
+}
+
