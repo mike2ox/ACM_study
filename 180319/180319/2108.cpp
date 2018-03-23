5014827d0f5e32e75da40b5ed7f75d378c9293c8
moonhyeok.song <firefinger07@gmail.com>
2018-03-23 11:32:34 +0900

    [BOJ]2108 - unsolved

---
+++ 180319/180319/2108.cpp
@@ -0,0 +1,112 @@
+/*
+*	author : quisutdeus7
+*	BJO : no. 2108
+*	status : solved
+*/
+#include<iostream>
+#include<math.h>
+using namespace std;
+int check[8001];
+
+int* insertsort(int arr[], int n_arr) {
+	int key = 0;
+	int pre, next;
+
+	for (next = 1; next < n_arr; next++) {
+		key = arr[next];
+		pre = next - 1;
+
+		while (arr[pre] > key && pre >= 0) {
+			arr[pre + 1] = arr[pre];
+			pre--;
+		}
+		arr[pre + 1] = key;
+	}
+
+	return arr;
+}
+void printresult(int* p_arr, int n_arr) {
+
+	for (int i = 0; i < n_arr; i++)
+		cout << p_arr[i] << "\n";
+}
+int arithmetic_m(int arr[], int n_arr) {
+	int mean = 0;
+	for (int a = 0; a < n_arr; a++)
+		mean += arr[a];
+	mean = ((double)mean / (double)n_arr);
+	return ceil(mean);
+}
+
+int middle_val(int arr[], int n_arr) {
+	int* arranged = insertsort(arr, n_arr);
+	// printresult(arranged, n_arr);
+	int mid = 0;
+	int mid1 = 0;
+
+	if (n_arr % 2 == 1)
+		mid = arranged[n_arr / 2];
+	else {
+		mid = arranged[(n_arr - 1) / 2];
+		mid1 = arranged[n_arr / 2];
+		int t = arithmetic_m(arranged, n_arr);
+		if (abs(t - mid) > abs(t - mid1))
+			mid = mid1;
+	}
+
+	return mid;
+}
+int mode_val(int arr[], int n_arr) {
+	int max=0, sec_max=0;
+	for (int a = 0; a < n_arr; a++) {
+		check[4000 + arr[a]]++;
+	}
+	for (int b = 8000; b >0; b--) {
+		if (check[b] != 0 && max < check[b])
+			max = b;
+		if (check[b] != 0 && max == check[b]) {
+			sec_max = max;
+			max = b;
+		}
+	}
+	if (sec_max == max)
+		return sec_max-4000;
+	else
+		return max-4000;
+}
+int range(int arr[], int n_arr) {
+	int* arranged = insertsort(arr, n_arr);
+	int scale = abs(arranged[n_arr - 1] - arranged[0]);
+
+	return scale;
+}
+
+int main() {
+	int n_arr;
+	int e_arr = 0;
+
+	cin >> n_arr;
+	int* arr = new int[n_arr];
+
+	if (n_arr <= 5e+5 && n_arr >= 1) {
+
+		for (int i = 0; i < n_arr; i++) {
+			cin >> e_arr;
+			if (abs(e_arr) <= 4e+3)
+				arr[i] = e_arr;
+			else
+				i--;
+		}
+		system("cls");
+
+		int a_mean = arithmetic_m(arr, n_arr);
+		int mid = middle_val(arr, n_arr);
+		int mode = mode_val(arr, n_arr);
+		int scale = range(arr, n_arr);
+
+		cout << a_mean << '\n' << mid << '\n' << mode << '\n' << scale;
+
+		delete arr;
+	}
+	return 0;
+}
