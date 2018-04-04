617e2b2e84da11631414a3a06c9c877ded515ef6
moonhyeok.song <firefinger07@gmail.com>
2018-04-04 19:27:59 +0900

    [BJO]1005 - unsolved

---
+++ 180328/180328/1005.cpp
@@ -0,0 +1,22 @@
+/*
+*	author : quisutdeus7
+*	BJO : 1005
+*	status : unsolved
+*/
+#include <iostream>
+using namespace std;
+
+int n_T;			// number of test case
+int N, K;			//건물수(<=1000) , 건물간 순서 규칙 수(<=100000)
+int t_result[1001];		//건물당 최소 완성 시간
+int pre_b[1000];		//이전 건물
+int next_b[1000];		//이후 건물
+
+
+int main() {
+	cin >> n_T;
+	cin >> N >> K;
+
+
+	return 0;
+}
