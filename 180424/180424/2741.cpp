3fa30a2bbd66a4643a83fa68ce53db4d4411e716
moonhyeok.song <firefinger07@gmail.com>
2018-04-24 19:17:12 +0900

    [BOJ]2741

---
+++ 180424/180424/2741.cpp
@@ -0,0 +1,18 @@
+/*
+*	author : quisutdeus7
+*	BJO : no. 2741
+*	status : solved
+*/
+#include<iostream>
+using namespace std;
+
+int N;
+
+int main() {
+	
+	cin >> N;
+	for (int i = 0; i < N; i++)
+		cout << i + 1 << '\n';
+
+	return 0;
+}
