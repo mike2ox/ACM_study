4e763e2ccf3aec59dc415d40a8c3dd64b9b190af
moonhyeok.song <firefinger07@gmail.com>
2018-04-24 19:13:47 +0900

    [BOJ]11719

---
+++ 180424/180424/11719.cpp
@@ -0,0 +1,23 @@
+/*
+*	author : quisutdeus7
+*	BJO : no. 11719
+*	status : solved(but used STL)
+*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+char input[101];	//input data
+int a;
+
+int main() {
+	while (1) {
+		a = getchar();	//STL!!!
+
+		if (a == EOF)
+			break;
+		putchar(a);		//STL!!
+
+	}	
+	return 0;
+}
