5304f7351df85af6b06358036924db3bd5fed28b
moonhyeok.song <firefinger07@gmail.com>
2018-04-24 19:14:26 +0900

    [BOJ]2438

---
+++ 180424/180424/2438.cpp
@@ -0,0 +1,22 @@
+/*
+*	author : quisutdeus7
+*	BJO : no. 2438
+*	status : solved
+*/
+#include<iostream>
+using namespace std;
+
+int n;		
+
+int main() {
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i+1; j++)
+			cout << "*";
+		cout << "\n";
+	}
+		
+
+	return 0;
+}
