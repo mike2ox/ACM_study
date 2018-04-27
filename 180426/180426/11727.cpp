b23a121fbfd27eb6e2093d1690068285cfb6e739
moonhyeok.song <firefinger07@gmail.com>
2018-04-27 11:31:14 +0900

    [BOJ]11727

---
+++ 180426/180426/11727.cpp
@@ -0,0 +1,22 @@
+/*
+*	author : quisutdeus7
+*	BJO : no. 11727
+*	status :solve
+*	type : dynamic program[DP]
+*/
+#include <iostream>
+using namespace std;
+int dp[1001] = {};
+int n;
+
+int main() {
+	dp[0] = 1;
+	dp[1] = 1;
+	cin >> n;
+
+	for (int i = 2; i < n + 1; i++)
+		dp[i] = (dp[i - 1] + 2*dp[i - 2]) % 10007;
+
+	cout << dp[n] % 10007;
+	return 0;
+}
