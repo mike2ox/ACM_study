c50fc02db475c60dcec3bd9999cf95d7ecd12068
moonhyeok.song <firefinger07@gmail.com>
2018-05-11 11:32:29 +0900

    [BOJ]2743

---
+++ 180430/180430/2743.cpp
@@ -0,0 +1,19 @@
+/*
+*	author : quisutdeus7
+*	BJO : no. 2743
+*	status : solve
+*	type : practice problem[PP]
+*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+string str;
+
+int main() {
+
+	cin >> str;
+	cout <<str.size() << '\n';
+
+	return 0;
+}
