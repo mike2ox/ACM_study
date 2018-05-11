d7c7e90837a62142569edc57eb3a91f58cfd1fe4
moonhyeok.song <firefinger07@gmail.com>
2018-05-11 11:33:37 +0900

    [BOJ]1406

---
+++ 180430/180430/1406.cpp
@@ -0,0 +1,16 @@
+/*
+*	author : quisutdeus7
+*	BJO : no. 1406
+*	status : unsolve
+*	type : practice problem[PP]
+*/
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+int main() {
+
+	return 0;
+}
