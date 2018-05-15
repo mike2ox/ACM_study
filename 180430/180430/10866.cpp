2708e130fe055aa94cd9af3afc5163e91bd66c73
moonhyeok.song <firefinger07@gmail.com>
2018-05-15 18:47:14 +0900

    [BOJ]10866

--- 180430/180430/10866.cpp
+++ 180430/180430/10866.cpp
@@ -4,75 +4,166 @@
 *	status : solved
 */
 #include<iostream>
-#include<string.h>
+#include<string>
 using namespace std;
 
-int stack[100001];
-int cnt_node = 0;
 int node_value;		//정수
 int n_func;			//number of functions
-int n_top;			//최상단 정수
 
-char command[6];
+string command;
+struct node {
+	int value;
+	node* pre;
+	node* next;
+};
 
-void push(int num);
-void pop();
-void size();
-void top();
-void empty();
+
+struct deque {
+	node* head;
+	node* tail;
+	int cnt;
+};
+
+void size(deque *dq);
+void front(deque *dq);
+void back(deque *dq);
+void empty(deque *dq);
+void push_front(deque *dq);
+void push_back(deque *dq);
+void pop_front(deque *dq);
+void pop_back(deque *dq);
 
 int main() {
+	deque DQ;
+	DQ.head = NULL;
+	DQ.tail = NULL;
+	DQ.cnt = 0;
 
 	cin >> n_func;
 
 	for (int a = 0; a < n_func; a++) {
 		cin >> command;
 
-		if (strcmp(command, "push") == 0) {
-			cin >> node_value;
-			push(node_value);
+		if (command == "push_front") {
+			push_front(&DQ);
 		}
-		else if (strcmp(command, "pop") == 0) {
-			pop();
+		else if (command == "push_back") {
+			push_back(&DQ);
 		}
-		else if (strcmp(command, "size") == 0) {
-			size();
+		else if (command == "pop_front") {
+			pop_front(&DQ);
 		}
-		else if (strcmp(command, "empty") == 0) {
-			empty();
+		else if (command == "pop_back") {
+			pop_back(&DQ);
 		}
-		else if (strcmp(command, "top") == 0) {
-			top();
+		else if (command == "size") {
+			size(&DQ);
+		}
+		else if (command == "empty") {
+			empty(&DQ);
+		}
+		else if (command == "front") {
+			front(&DQ);
+		}
+		else if (command == "back") {
+			back(&DQ);
 		}
 	}
 
 	return 0;
 }
-void push(int num) {
-	n_top++;
-	stack[n_top] = num;
-	return;
-}
-void pop() {
-	if (n_top > 0) {
-		cout << stack[n_top] << '\n';
-		n_top--;
+void push_front(deque *dq) {
+	node* temp = new node;
+	cin >> temp->value;
+	
+	if (dq->cnt == 0) {
+		dq->head = temp;
+		dq->tail = temp;
+		(dq->cnt)++;
 	}
-	else
-		cout << "-1\n";
+	else {
+		temp->next = dq->head;
+		dq->head->pre = temp;
+		temp->pre = NULL;
+		dq->head = temp;
+		(dq->cnt)++;
+	}
+
 }
-void size() {
-	cout << n_top << '\n';
+void push_back(deque *dq) {
+	node* temp = new node;
+	cin >> temp->value;
+
+	if (dq->cnt == 0) {
+		dq->head = temp;
+		dq->tail = temp;
+		(dq->cnt)++;
+	}
+	else {
+		temp->pre = dq->tail;
+		dq->tail->next = temp;
+		temp->next = NULL;
+		dq->tail = temp;
+		(dq->cnt)++;
+	}
 }
-void top() {
-	if (n_top<1)
-		cout << "-1\n";
-	else
-		cout << stack[n_top] << '\n';
+void pop_front(deque *dq) {
+	if(dq->cnt == 0)
+		cout << "-1" << '\n';
+	else if (dq->cnt == 1) {
+		node* temp = new node;
+		cout << dq->head->value << '\n';
+		(dq->cnt)--;
+		delete dq->head;
+	}
+	else {
+		node* temp = new node;
+		cout << dq->head->value << '\n';
+		temp = dq->head;
+		dq->head = dq->head->next;
+		dq->head->pre = NULL;
+		(dq->cnt)--;
+		delete temp;
+	}
 }
-void empty() {
-	if (n_top<1)
-		cout << "1\n";
+void pop_back(deque *dq) {
+	if (dq->cnt == 0) 
+		cout << "-1" << '\n';
+
+	else if (dq->cnt == 1) {
+		node* temp = new node;
+		cout << dq->tail->value << '\n';
+		(dq->cnt)--;
+		delete dq->tail;
+	}
+	else {
+		node* temp = new node;
+		cout << dq->tail->value << '\n'; 
+		temp = dq->tail;
+		dq->tail = dq->tail->pre;
+		dq->tail->next = NULL;
+		(dq->cnt)--;
+		delete temp;
+	}
+}
+void size(deque *dq) {
+	cout << dq->cnt << '\n';
+}
+void front(deque *dq) {
+	if (dq->cnt == 0)
+		cout << "-1" << '\n';
 	else
-		cout << "0\n";
+		cout << dq->head->value << '\n';
+}
+void back(deque *dq) {
+	if (dq->cnt == 0)
+		cout << "-1"<<'\n';
+	else
+		cout << dq->tail->value << '\n';
+}
+void empty(deque *dq) {
+	if (dq->cnt == 0)
+		cout << "1"<<'\n';
+	else
+		cout << "0"<<'\n';
 }
