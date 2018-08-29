6c9a940c887cc8f56d3a63f77a6c82f8b1089dcb
mike2ox <firefinger07@gmail.com>
2018-08-29 18:27:53 +0900

    [update] node형태의 백터로 사용시 BFS에서 segmentation fault

--- 180828/BJO/1260.cpp
+++ 180828/BJO/1260.cpp
@@ -7,10 +7,12 @@
 
 #include<iostream>
 #include<vector>
+#include<queue>
 using namespace std;
 
 typedef struct{
-    int ckpt =0;
+    int value;
+    bool ckpt =false;
     vector<int> edge;
 }node;
 
@@ -19,11 +21,11 @@ int V;
 
 void DFS(node* dfsbfs){
 
-    if((*dfsbfs).ckpt == -1)
+    if((*dfsbfs).ckpt == true)
         return;
     
-    cout << (*dfsbfs).ckpt << " ";
-    (*dfsbfs).ckpt = -1;
+    cout << (*dfsbfs).value << " ";
+    (*dfsbfs).ckpt = true;
 
     for(vector<int>::iterator itr = (*dfsbfs).edge.begin() ; itr != (*dfsbfs).edge.end(); ++itr){
         DFS(&DFSBFS[*itr]);
@@ -31,12 +33,28 @@ void DFS(node* dfsbfs){
 }
 // 또 막힘. BFS 구현 부분 추가 필요
 void BFS(node* dfsbfs){
-    vector<node*> temp;
-    cout << (*dfsbfs).ckpt << " ";
-    (*dfsbfs).ckpt = -1;
+    vector<node> temp;
+    cout << (*dfsbfs).value << " ";
+    (*dfsbfs).ckpt = true;
 
-    for(vector<int>::iterator itr = (*dfsbfs).edge.begin() ; itr != (*dfsbfs).edge.end(); ++itr)
-        cout << DFSBFS[*itr].ckpt;
+    for(vector<int>::iterator itr = (*dfsbfs).edge.begin() ; itr != (*dfsbfs).edge.end(); itr++){
+        cout << *itr << " ";
+        temp.push_back(dfsbfs[*itr]);
+        dfsbfs[*itr].ckpt = true;
+    }
+    
+    while(!temp.empty()){
+        for(vector<node>::iterator itr = temp.begin() ; itr != temp.end(); itr++){
+            if((*itr).ckpt)
+                continue;
+            cout << (*itr).value << " ";
+            (*dfsbfs).ckpt = true;
+            for(vector<int>::iterator itr2 = (*itr).edge.begin() ; itr2 != (*itr).edge.end(); ++itr2){
+                temp.push_back(dfsbfs[*itr2]);
+            }
+            temp.erase(itr);
+        }
+    }
     
 }
 
@@ -48,13 +66,16 @@ int main(){
 
     for(int x=1;x<=n_M;x++){
         cin >> N >> M;
-        DFSBFS[N].ckpt = N;
-        DFSBFS[M].ckpt = M;
+        DFSBFS[N].value = N;
+        DFSBFS[M].value = M;
         DFSBFS[N].edge.push_back(M);
         DFSBFS[M].edge.push_back(N);
     }
 
     DFS(&DFSBFS[V]);
+    cout << '\n';
+    for(int i=0;i<M;++i)
+        DFSBFS[i].ckpt = false;
     BFS(&DFSBFS[V]);
 
     return 0;
