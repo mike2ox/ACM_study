e47f6ddc0a019c0a9325007ff2f2f49eb27d1193
mike2ox <firefinger07@gmail.com>
2018-09-18 21:12:35 +0900

    [test] git 사용

---
+++ 180918/BJO/1707.cpp
@@ -0,0 +1,19 @@
+    /*
+*	author : mike2ox
+*	BJO : 이분그래프
+*	descript : https://www.acmicpc.net/problem/1707
+*	type : graph
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio("false");
+	cin >> n >> k;
+
+	system("pause");
+	return 0;
+}
