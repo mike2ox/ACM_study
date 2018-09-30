07e015c4d511360c42fc5190a7736db32e7765fa
mike2ox <firefinger07@gmail.com>
2018-09-30 19:18:59 +0900

    [update] 디렉토리 구조 변경, sort부분 다시 공부할 필요 있음.

---
+++ BJO_Greedy/11399.cpp
@@ -0,0 +1,40 @@
+/*
+*	author : mike2ox
+*	BJO : ATM
+*	descript : https://www.acmicpc.net/problem/11399
+*	type : greedy
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int n;
+int t_time = 0;
+
+bool comp(const int &a, const int &b) {
+	return a < b;
+}
+
+int main() {
+
+	ios_base::sync_with_stdio("false");
+	cin >> n;
+	vector<int> p(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> p[i];
+	}
+
+	sort(p.begin(), p.end(), comp);
+
+	for (int i = 0; i < n; i++) {
+		t_time += p[i];
+		for (int j = 0; j < i; j++) {
+			t_time += p[j];
+		}
+	}
+	cout << t_time << '\n';
+	system("pause");
+	return 0;
+}
